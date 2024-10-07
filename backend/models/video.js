e4764629dff41d73cc6e10998d301d1ed5a86ee6
IK <khushijukanti33@gmail.com>
2024-10-08 00:50:43 +0530

    add new like and dislike entities for video schema

--- backend/models/video.js
+++ backend/models/video.js
@@ -4,6 +4,8 @@ const videoSchema = new mongoose.Schema({
   title: String,
   description: String,
   filePath: String,
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
 });
 
