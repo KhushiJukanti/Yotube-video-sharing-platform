ef53298986fc7f74b575f8119074562d52d2e7e6
IK <khushijukanti33@gmail.com>
2024-10-09 12:41:35 +0530

    remove unwanted comments

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,6 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Create HTTP server and Socket.IO server
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: 'http://localhost:3000', // Adjust this for frontend
-//     methods: ['GET', 'POST'],
-//   },
-// });
 
 app.set('socketio', io)
 
@@ -56,20 +48,6 @@ mongoose.connect(process.env.Mongo_Uri).then(() => {
 
 
 
-
-
-// // Socket.IO logic
-// io.on('connection', (socket) => {
-//   console.log('User connected:', socket.id);
-
-//   socket.on('new_comment', (comment) => {
-//     io.emit('receive_comment', comment); // Broadcast new comment to all connected clients
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id);
-//   });
-// });
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/videos', videoRoutes);
