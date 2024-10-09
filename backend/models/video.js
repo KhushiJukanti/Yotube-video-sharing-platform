e755394f8486723c39b3c1a3a4716bec7bb1381c
IK <khushijukanti33@gmail.com>
2024-10-09 12:04:03 +0530

    update videoschema model with videourl, description, like, dislike, uploded date, and channel name

--- backend/models/video.js
+++ backend/models/video.js
@@ -1,12 +1,41 @@
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  filePath: String,
-  likes: { type: Number, default: 0 },
-  dislikes: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
+  title: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,  // Added dislikes field
+    default: 0,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
+  channelName: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('Video', videoSchema);
